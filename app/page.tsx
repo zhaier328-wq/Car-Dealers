3d241b9bece8146d603fc775481db0f2e29d7b24
zohair <zohairh01@gmail.com>
2026-05-23 07:40:46 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-5xl font-bold">
+        Car Dealer Website
+      </h1>
+
+      <p className="mt-4 text-lg">
+        My first Next.js automotive project
+      </p>
+    </div>
+  );
+}
